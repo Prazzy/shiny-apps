c9121793e7024e1ad32ccabe772f942c47d3a001
Praveen <prazzee@gmail.com>
2014-03-21 08:02:05 -0700

    Ractive example

---
+++ apps/reactive-example/server.R
@@ -0,0 +1,68 @@
+library(shiny)
+library(rCharts)
+library(DBI)
+library(RMySQL)
+
+newval <- function(n) {
+  drv = dbDriver('MySQL')
+  con = dbConnect(drv, dbname='mydb',user='dbuser',pass='dbuser')
+  res = dbGetQuery(con, statement="select * from polls_choice where poll_id=1")
+  choice <- c(res['choice_text'])
+  votes <- c(res['votes'])
+  dbDisconnect(con)
+}
+
+shinyServer(function(input, output) {
+  
+  output$distPlot <- renderPlot({
+    x    <- faithful[, 2]  # Old Faithful Geyser data
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+  })
+  
+  output$myChart1 <- renderChart2({
+    h1 <- Highcharts$new()
+    h1$chart(type = "column")
+    h1$series(data = c(input$bins, 3, 4), name="Browser Share")
+    h1$legend(symbolWidth = 80)
+    #h1$set(dom = "myChart")
+    return(h1)
+  })
+  
+  #print(input$inputID)
+  drv = dbDriver('MySQL')
+  con = dbConnect(drv, dbname='mydb',user='dbuser',pass='dbuser')
+  res = dbGetQuery(con, statement="select * from polls_choice")
+  choice <- c(res['choice_text'])
+  votes <- c(res['votes'])
+  dbDisconnect(con)
+  
+  output$myChart <- renderChart2({
+    #h1 <- Highcharts$new()
+    #h1$chart(type = "column")
+    #h1$series(data = vals$votes, name="Browser Share")
+    #h1$legend(symbolWidth = 80)
+    #return(h1)
+    x <- data.frame(key = choice$choice_text, value = votes$votes)
+    h2 <- hPlot(x = "key", y = "value", data = x, type = "pie")
+    #h2 <- Highcharts$new()
+    #h2$chart(type = "pie")
+    #h2$series(data = c(x)$value, name="Browser Share")
+    h2$title(text = "Browser market shares at a specific website, 2010")
+    h2$tooltip(pointFormat = '<b>{point.percentage:.1f}%</b>')
+    h2$plotOptions(pie=list(
+        allowPointSelect = TRUE,
+        cursor = 'pointer',
+        dataLabels =  list(
+          enabled = TRUE,
+          color = '#000000',
+          connectorColor = '#000000',
+          format=  '<b>{point.name}</b>: {point.percentage:.1f} %'
+        )
+    )
+    )
+    return(h2)
+  })
+})
---
+++ apps/reactive-example/ui.R
@@ -0,0 +1,38 @@
+library(shiny)
+require(rCharts)
+shinyUI(pageWithSidebar(
+  headerPanel("rCharts: Highcharts"),
+  sidebarPanel(
+    selectInput(inputId = "x",
+                label = "Choose X",
+                choices = c(1, 2),
+                selected = "2",
+                multiple = FALSE),
+    sliderInput("bins",
+                "Number of bins:",
+                min = 1,
+                max = 50,
+                value = 30)
+  ),
+  
+  mainPanel(showOutput("myChart", "Highcharts"), 
+            showOutput("myChart1", "Highcharts"),
+            plotOutput("distPlot")
+  )
+))
+
+# Define UI for application that draws a histogram
+shinyUI(pageWithSidebar(
+  headerPanel("rCharts: Highcharts"),
+  # Sidebar with a slider input for the number of bins
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
+    # Show a plot of the generated distribution
+    mainPanel(plotOutput("distPlot")
+    )
+))
